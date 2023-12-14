6b039b61fc2aaad0e327a1a8d7c6a8a38c96f164
Sascha Koch <koch@eas-gmbh.net>
2023-12-14 18:35:03 +0100

    CI/CD successful

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require __DIR__ . '/vendor/autoload.php';
 echo "Project cloned to VPS successfully!";
 echo "<br>";
 echo "Project setup for CI/CD successfully";
+echo "<br>";
+echo "Test";
 
 //$api = ApiRequest::getInstance();
 //$api->getASchemaForMultipleTables();
